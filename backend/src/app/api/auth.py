081954682ec95c3768bc9b12afafa8a7a748d217
Mercuuury <kovshiksa@gmail.com>
2024-11-30 19:30:32 +0300

    fix `auth.py@register`

--- backend/src/app/api/auth.py
+++ backend/src/app/api/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from app.models.user import User, Token
+from app.models.user import UserCreate, Token
 from app.db import database, users
 from app.crud.auth import hash_password, verify_password
 from app.crud.user import create_access_token
@@ -7,7 +7,7 @@ from app.crud.user import create_access_token
 router = APIRouter()
 
 @router.post("/register", status_code=201)
-async def register(user: User):
+async def register(user: UserCreate):
     query = users.select().where(users.c.email == user.email)
     existing_user = await database.fetch_one(query)
     if existing_user:
@@ -16,8 +16,10 @@ async def register(user: User):
     hashed_password = hash_password(user.password)
     user_data = user.dict()
     user_data['password'] = hashed_password
-    query = users.insert().values()
-    return {"message": "User registered successfully"}
+    query = users.insert().values(user_data)
+    user = await database.fetch_one(query)
+    
+    return {"message": "User registered successfully", "user": user}
 
 # @router.post("/login", response_model=Token)
 # async def login(user: User):
