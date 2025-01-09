8b5e914300cb99e95b7066ea2968e6248f692aeb
Tatiana Andreeva <snurnisyan@gmail.com>
2025-01-09 21:16:56 +0300

    feat: add search string functionality

--- frontend/src/Api/Vacancies/Vacancies.ts
+++ frontend/src/Api/Vacancies/Vacancies.ts
@@ -3,8 +3,8 @@ import paths from './paths';
 import {iApi} from './types';
 
 export default {
-    getVacancies: async () => {
-        return await axios.get<iApi.oVacancies>(paths.getVacancies);
+    getVacancies: async (params: iApi.iParams) => {
+        return await axios.get<iApi.oVacancies>(paths.getVacancies, { params });
     },
     getVacancyById: async (id: string) => {
         return await axios.get<iApi.oVacancy>(paths.getVacancy + id);
--- frontend/src/pages/vacancies/index.page.tsx
+++ frontend/src/pages/vacancies/index.page.tsx
@@ -1,15 +1,23 @@
 import {App} from '@/Types';
 import Default from '@/Layouts/Default/Default';
-import {Flex} from "@chakra-ui/react";
+import {Flex, Text} from "@chakra-ui/react";
 import styles from './index.module.scss';
 import SearchHeader from "@/pages/vacancies/Modules/SearchHeader";
 import {useVacancies} from "@/pages/vacancies/Hooks/useVacancies";
 import VacancyTabs from "@/pages/vacancies/Modules/VacancyTabs";
 import {useEvents} from "@/pages/vacancies/Hooks/useEvents";
+import {useEffect, useState} from "react";
+import {iQueryParams, iVacancy} from "@/pages/vacancies/Types/types";
 
 const IndexPage:App.Next.NextPage = () => {
-    const {data: vacancies, isPending: vacanciesLoading} = useVacancies();
+    const [vacancies, setVacancies] = useState<Array<iVacancy> | undefined>();
+    const [searchParams, setSearchParams] = useState<iQueryParams>({ enabled: true });
+    const {data: vacanciesData, isPending: vacanciesLoading} = useVacancies(searchParams);
     const {data: events, isPending: eventsLoading} = useEvents();
+
+    useEffect(() => {
+        setVacancies(vacanciesData)
+    }, [vacanciesData])
 
     return (
         <Flex
@@ -22,13 +30,23 @@ const IndexPage:App.Next.NextPage = () => {
                 flexDirection={"column"}
                 width={"60%"}
             >
-                <SearchHeader />
-                <VacancyTabs
-                    vacanciesLoading={vacanciesLoading}
-                    vacancies={vacancies}
-                    eventsLoading={eventsLoading}
-                    events={events}
-                />
+                <SearchHeader setSearchParams={setSearchParams} isLoading={vacanciesLoading} />
+                {vacancies && vacancies.length === 0 ? (
+                    <Flex
+                        mt={"40px"}
+                        justifyContent={"center"}
+                        width={"100%"}
+                    >
+                        <Text fontStyle={"italic"}>Вакансии по выбранным параметрам не найдены</Text>
+                    </Flex>
+                ) : (
+                    <VacancyTabs
+                        vacanciesLoading={vacanciesLoading}
+                        vacancies={vacancies}
+                        eventsLoading={eventsLoading}
+                        events={events}
+                    />
+                )}
             </Flex>
         </Flex>
     )
